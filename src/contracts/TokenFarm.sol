92d459f0bed1f026acebe5ee937e6fbe33dc3338
Pichi Loco <matiasdmansilla@hotmail.com>
2021-02-22 00:18:37 -0300

    smart contracts & tests ready

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,12 +5,72 @@ import "./DaiToken.sol";
 
 contract TokenFarm{
     string public name = "Be Token Farm";
+    address public owner;
     BeToken public beToken;
     DaiToken public daiToken;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
 
     constructor (BeToken _beToken, DaiToken _daiToken) public {
         beToken = _beToken;
         daiToken = _daiToken;
+        owner = msg.sender;
+    }
+
+    function stakeTokens(uint _amount) public {
+        
+        // Require amount greater than 0
+        require(_amount>0, 'amount cannot be 0');
+
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update staking balance
+        stakingBalance[msg.sender] += _amount;
+
+        // Add user to stakers array *only* if they haven't staked already
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    function issueTokens() public {
+        // Only owner can call this function
+        require(msg.sender == owner, 'caller must be the owner');
+
+        // Issue tokens to all stakers
+        for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if (balance!=0) {
+                beToken.transfer(recipient, balance);
+            }
+        }
+    }
+
+    function unstakeTokens() public {
+        
+        // Fetch staking balance
+        uint balance = stakingBalance[msg.sender];
+
+        // Require amount greater than 0
+        require(balance > 0, 'staking balance cannot be 0');
+
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transfer(msg.sender, balance);
+
+        // Reset staking balance
+        stakingBalance[msg.sender] = 0;
+
+        // Update staking status
+        isStaking[msg.sender] = false;
+
     }
 
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -52,4 +52,58 @@ contract('TokenFarm', ([owner, investor]) => {
         })
     })
 
+    describe('Farming tokens', async () => {
+        
+        it('rewards investors for staking mDai tokens', async () => {
+            let result
+
+            // Check investor balance before staking
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance check before staking')
+
+            // Stake Mock DAI Tokens
+            await daiToken.approve(tokenFarm.address, tokens('100'), { from: investor })
+            await tokenFarm.stakeTokens(tokens('100'), { from: investor })
+
+            // Check staking result
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), '0', 'investor Mock DAI wallet balance correct after staking')
+
+            result = await daiToken.balanceOf(tokenFarm.address)
+            assert.equal(result.toString(), tokens('100'), 'Token Farm Mock DAI wallet balance correct after staking')
+
+            result = await tokenFarm.stakingBalance(investor)
+            assert.equal(result.toString(), tokens('100'), 'inversor staking balance correct after staking')
+
+            result = await tokenFarm.isStaking(investor)
+            assert.equal(result.toString(), 'true', 'inversor staking status correct after staking')
+
+            // Issue Tokens
+            await tokenFarm.issueTokens({ from: owner })
+
+            // Check balances after issuance
+            result = await beToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor DApp Token wallet balance correct after issuance');
+
+            // Ensure that only owner can issue tokens
+            await tokenFarm.issueTokens({ from: investor }).should.be.rejected;
+
+            // Unstake tokens
+            await tokenFarm.unstakeTokens({ from: investor })
+
+            // Check results after unstaking
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance correct after unstaking')
+
+            result = await daiToken.balanceOf(tokenFarm.address)
+            assert.equal(result.toString(), '0', 'Token Farm Mock DAI balance correct after unstaking')
+
+            result = await tokenFarm.stakingBalance(investor)
+            assert.equal(result.toString(), '0', 'investor staking balance correct after unstaking')
+
+            result = await tokenFarm.isStaking(investor)
+            assert.equal(result.toString(), 'false', 'inversor staking status correct after unstaking')
+        })
+    })
+
 })
